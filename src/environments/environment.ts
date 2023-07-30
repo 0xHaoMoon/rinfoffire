aa81212a59d8618809a5e3e7084ea8ec425667e8
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-30 22:18:24 +0200

    add firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ringoffire-affc0',
+    appId: '1:79459347376:web:ea58175fb392f8ec158475',
+    storageBucket: 'ringoffire-affc0.appspot.com',
+    apiKey: 'AIzaSyAo6I4a425Ur4ezjJ44mSmsjEfxTaXJ0Go',
+    authDomain: 'ringoffire-affc0.firebaseapp.com',
+    messagingSenderId: '79459347376',
+  },
+};
