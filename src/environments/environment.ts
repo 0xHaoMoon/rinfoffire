2c78376d626e405d938a4a580f96e4a7478ff0f8
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-08-03 21:59:36 +0200

    responsive start screen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   firebase: {
     projectId: 'ringoffire-affc0',
-    appId: '1:79459347376:web:ea58175fb392f8ec158475',
+    appId: '1:79459347376:web:5da041bcd3813d0e158475',
+    databaseURL: 'https://ringoffire-affc0-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ringoffire-affc0.appspot.com',
     apiKey: 'AIzaSyAo6I4a425Ur4ezjJ44mSmsjEfxTaXJ0Go',
     authDomain: 'ringoffire-affc0.firebaseapp.com',
